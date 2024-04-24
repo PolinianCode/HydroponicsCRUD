7f28ff468c9cac5d1db87431116d235573e6b55b
Danylo Koval <37926017+PolinianCode@users.noreply.github.com>
2024-04-24 19:23:22 +0200

    Validation for hydroponic system name

--- HydroponicsCRUD/hydroponic_system/serializer.py
+++ HydroponicsCRUD/hydroponic_system/serializer.py
@@ -6,3 +6,8 @@ class HydroponicSystemSerializer(serializers.ModelSerializer):
     class Meta:
         model = HydroponicSystem
         fields = ['id', 'owner', 'name']
+
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Name is too short")
+        return value
